fbd9ca9925b75b0eb07aa60d2dda3f48b49770c6
sourav <samantasourav184@gamil.com>
2022-03-12 23:25:01 +0530

    api part added

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,21 +1,33 @@
 
 const AppReducer = (state, action) => {
-    let res = {}
+    let res = { ...state }
     switch (action.type) {
+        case 'GET_TRANSACTIONS':
+            res = {
+                ...state,
+                loading: false,
+                transactions: action.transactions
+            }
         case 'DELETE_TRANSACTION':
             res = {
                 ...state, transactions: state.transactions.filter(transaction => transaction.id !== action.delId)
             }
             break
         case 'ADD_TRANSACTION':
-            // state.transactions.push(action.addTransaction)
             res = {
                 ...state, transactions: [action.addTransaction, ...state.transactions]
             }
             break
         case 'EDIT_TRANSACTION':
-            res = state
+            res = {
+                ...state, transactions: state.transactions.map(item => item.id === action.editId ? { id: action.editId, ...action.newTransaction } : item)
+            }
             break
+        case 'TRANSACTION_ERROR':
+            return {
+                ...state,
+                error: action.errors
+            }
         default:
             res = state;
     }
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,14 +1,20 @@
-import React, { createContext, useReducer } from "react";
-import AppReducer from "./AppReducer";
+import React, { createContext, useReducer } from "react"
+import AppReducer from "./AppReducer"
 //Initial State
 
+// const initialState = {
+//     transactions: [
+//         { id: 1, type: 'expense', amount: 150, note: 'Full day expense' },
+//         { id: 2, type: 'income', amount: 1000, note: 'This month income' },
+//         { id: 3, type: 'debt', amount: 100, note: 'Debt from someone' },
+//         { id: 4, type: 'lend', amount: 230, note: 'Lend from someone' },
+//     ]
+// }
+
 const initialState = {
-    transactions: [
-        { id: 1, type: 'expense', amount: 150, note: 'Full day expense' },
-        { id: 2, type: 'income', amount: 1000, note: 'This month income' },
-        { id: 3, type: 'debt', amount: 100, note: 'Debt from someone' },
-        { id: 4, type: 'lend', amount: 230, note: 'Lend from someone' },
-    ]
+    transactions: [],
+    error: null,
+    loading: true
 }
 
 //Create context 
@@ -18,22 +24,99 @@ export const GlobalContext = createContext(initialState)
 //Provider components
 export const GlobalProvider = ({ children }) => {
     const [state, dispatch] = useReducer(AppReducer, initialState)
+    const headers = {
+        "Authorization": "Basic Y2RoYnV5dGdleGpudXVpMnRiMTYzNXg2NGozY2lvMnk4Yzc1OTZxbjl2dDQ2OiQyeSQxMCR2WVRidHN3UG02YWVac01RUmN2akNlS0FvTi5ObVY0WVljdmh3cjRUY2lFY043bG11YnVqQw==",
+        "Content-Type": "application/json"
+    }
+    const callApi = async (url = '', method = 'GET', headers, body = null) => {
+        try {
+            const res = await fetch(url, {
+                method: method,
+                body: body,
+                headers: headers
+            })
+            let data = await res.json()
+            return { data: data, err: null }
+        } catch (err) {
+            return { data: null, err: err }
+        }
+    }
+    const getTransactions = () => {
+        let { err, data } = callApi('/api/v2/transactions', 'GET', headers)
+        if (data) {
+            dispatch({
+                type: 'GET_TRANSACTIONS',
+                transactions: data
+            })
+        } else {
+            dispatch({
+                type: 'TRANSACTION_ERROR',
+                errors: err
+            })
+        }
+    }
+    const getTransaction = (id) => {
+        let { err, data } = callApi('/api/v2/transactions/' + id, 'GET', headers)
+        if (data)
+            dispatch({
+                type: 'GET_TRANSACTION',
+                transactions: data
+            })
+        else
+            dispatch({
+                type: 'TRANSACTION_ERROR',
+                errors: err
+            })
+
+    }
     const deleteTransaction = (id) => {
-        dispatch({ type: 'DELETE_TRANSACTION', delId: id })
+        let { err, data } = callApi('/api/v2/transactions/' + id, 'DELETE', headers)
+        if (data)
+            dispatch({
+                type: 'DELETE_TRANSACTION',
+                delId: id
+            })
+        else
+            dispatch({
+                type: 'TRANSACTION_ERROR',
+                errors: err
+            })
     }
     const addTransaction = (transaction) => {
-        dispatch({ type: 'ADD_TRANSACTION', addTransaction: transaction })
+        let { err, data } = callApi('/api/v2/transactions', 'POST', headers, transaction)
+        if (data)
+            dispatch({ type: 'ADD_TRANSACTION', addTransaction: transaction })
+        else
+            dispatch({
+                type: 'TRANSACTION_ERROR',
+                errors: err
+            })
     }
     const editTransaction = (id, transaction) => {
-        dispatch({ type: 'EDIT_TRANSACTION', editId: transaction, newTransaction: transaction })
+        let { err, data } = callApi('/api/v2/transactions/' + id, 'PUT', headers, transaction)
+        if (data)
+            dispatch({
+                type: 'EDIT_TRANSACTION',
+                editId: id,
+                newTransaction: transaction
+            })
+        else
+            dispatch({
+                type: 'TRANSACTION_ERROR',
+                errors: err
+            })
     }
     return (
         <GlobalContext.Provider
             value={{
                 transactions: state.transactions,
-                deleteTransaction,
+                error: state.error,
+                loading: state.loading,
+                getTransactions,
+                getTransaction,
                 addTransaction,
-                editTransaction
+                editTransaction,
+                deleteTransaction
             }}
         >
             {children}
